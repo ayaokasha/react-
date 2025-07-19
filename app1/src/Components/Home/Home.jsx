6c55d4f4375710bad55281867d677734c65c66c0
Aya Okasha <ayaskasha219@gmail.com>
2025-07-19 04:20:44 +0300

    react

--- app1/src/Components/Home/Home.jsx
+++ app1/src/Components/Home/Home.jsx
@@ -1,23 +1,32 @@
-
-import React, { useState } from 'react'
-import Navbar from '../NavBar/Navbar'
+import React, { useState } from "react";
+import Navbar from "../NavBar/Navbar";
 
 export default function Home() {
-    
-//hook
-//useState is a hook that allows us to add state to functional components.
-const [count , setCount] = useState(0);
+  //hook
+  //useState is a hook that allows us to add state to functional components.
+  const [count, setCount] = useState(0);
 
-const [userName, setUserName] = useState("Ahmed");
+  const [userName, setUserName] = useState("Ahmed");
+  const [darkStyle, setdarkStyle] = useState("bg-dark text-light p-2");
 
+  function changeCount() {
+    setCount(Math.floor(Math.random() * 100));
+  }
 
+  return (
+    <>
+      <Navbar />
+      <h2 className={darkStyle}>{userName}</h2>
 
+      <h2 className={count > 50 ? "bg-success" : "bg-danger"}>
+        count : {count}
+      </h2>
 
+      <button onClick={changeCount} className="btn btn-success">
+        change count
+      </button>
 
-
-
-  return <>
-  <Navbar/>
-  <h2 className='fas fa-home '> Home Component</h2>
-  </>
+      <h2 className="fas fa-home"> Home Component</h2>
+    </>
+  );
 }
